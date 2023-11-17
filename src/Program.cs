e9f0a8c758fc5c6644dbb9c9985e24a223a3edd5
Gustavo César de Melo <gustavo.cesar@websis.com.br>
2023-11-17 15:17:37 -0300

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+using LogConverter;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
