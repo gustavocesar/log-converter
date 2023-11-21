e613d51101092dbe6ecbe6b0735505fdafa76ec2
Gustavo César de Melo <gustavo.cesar@websis.com.br>
2023-11-20 21:05:13 -0300

    Add method GetFileContents

--- src/Domain/Interfaces/IDownloadFileService.cs
+++ src/Domain/Interfaces/IDownloadFileService.cs
@@ -2,4 +2,5 @@
 
 public interface IDownloadFileService
 {
+    Task<string> GetFileContents(string url);
 }
--- src/Infrastructure/Services/DownloadFileService.cs
+++ src/Infrastructure/Services/DownloadFileService.cs
@@ -4,4 +4,12 @@ namespace LogConverter.Infrastructure.Services;
 
 public class DownloadFileService : IDownloadFileService
 {
+    public async Task<string> GetFileContents(string url)
+    {
+        using var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        var response = await client.SendAsync(request);
+
+        return await response.Content.ReadAsStringAsync();
+    }
 }
