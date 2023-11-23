a880e78d7bdad044392e626e66cbb28c44fd3c00
Gustavo César de Melo <gustavo.cesar@websis.com.br>
2023-11-23 19:47:13 -0300

    Log conversion (use case)

--- src/UseCases/ConvertMinhaCdnToAgoraUseCase.cs
+++ src/UseCases/ConvertMinhaCdnToAgoraUseCase.cs
@@ -1,13 +1,43 @@
-﻿using LogConverter.UseCases.Interfaces;
+﻿using LogConverter.Domain.Entities;
+using LogConverter.Domain.Interfaces;
+using LogConverter.UseCases.Interfaces;
 
 namespace LogConverter.UseCases;
 
 public class ConvertMinhaCdnToAgoraUseCase : IConvertMinhaCdnToAgoraUseCase
 {
+    private readonly IFileService _fileService;
+
+    public ConvertMinhaCdnToAgoraUseCase(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+
     public async Task ExecuteAsync(string sourceUrl, string destinationPath)
     {
-        //TODO
+        var minhaCdnLines = await _fileService.DownloadFileContents(sourceUrl);
 
-        await Task.CompletedTask;
+        var listMinhaCdn = minhaCdnLines.Select(line => (MinhaCdn)line);
+        var listAgora = listMinhaCdn.Select(log => (Agora)log);
+
+        await CreateLogFileAgoraFormat(listAgora, destinationPath);
+    }
+
+    private async Task CreateLogFileAgoraFormat(IEnumerable<Agora> logAgoras, string destinationPath)
+    {
+        //header
+        var output = new List<string>
+        {
+            "#Version: 1.0",
+            "#Date: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"),
+            "#Fields: provider http-method status-code uri-path time-taken response-size cache-status"
+        };
+
+        //body
+        output.AddRange(logAgoras.Select(log => log.ToString()));
+
+        _fileService.CreatePathIfNotExists(destinationPath);
+
+        await File.WriteAllLinesAsync(destinationPath, output);
     }
 }
