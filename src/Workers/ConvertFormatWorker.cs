fbae88d7e49b828fa968f273aa4c1b30aa596239
Gustavo César de Melo <gustavo.cesar@websis.com.br>
2023-11-23 14:52:15 -0300

    Exception handling

--- src/Workers/ConvertFormatWorker.cs
+++ src/Workers/ConvertFormatWorker.cs
@@ -7,28 +7,35 @@ public class ConvertFormatWorker : BackgroundService
     private readonly ILogger<ConvertFormatWorker> _logger;
     private readonly IHostApplicationLifetime _hostApplicationLifetime;
     private readonly IConvertMinhaCdnToAgoraUseCase _useCase;
+    private readonly IList<string> _arg;
 
     public ConvertFormatWorker(ILogger<ConvertFormatWorker> logger,
         IHostApplicationLifetime hostApplicationLifetime,
-        IConvertMinhaCdnToAgoraUseCase useCase
+        IConvertMinhaCdnToAgoraUseCase useCase,
+        IList<string> arg
     )
     {
         _logger = logger;
         _hostApplicationLifetime = hostApplicationLifetime;
         _useCase = useCase;
+        _arg = arg;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-
         try
         {
-            //todo
+            if (!_arg.Any() || _arg.Count < 2)
+                throw new Exception("Please provide URL (sourceUrl) and a destination file (targetPath) as arguments.");
+
+            var sourceUrl = _arg.FirstOrDefault() ?? string.Empty;
+            var targetPath = _arg.LastOrDefault() ?? string.Empty;
+
+            //TODO
         }
         catch (Exception e)
         {
             _logger.LogError($"Exception: {e.Message} | {e.StackTrace}");
-            throw;
         }
         finally
         {
